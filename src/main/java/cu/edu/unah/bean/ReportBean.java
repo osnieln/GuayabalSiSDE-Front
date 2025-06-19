d7d2472ec22aff496761ce6d0364be31e785e1f9
Gabriel Alberto Pérez Guerra <gabrielpga20@gmail.com>
2025-06-19 14:30:38 -0400

    Adicionados reportes

---
+++ src/main/java/cu/edu/unah/bean/ReportBean.java
@@ -0,0 +1,82 @@
+package cu.edu.unah.bean;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Named
+@Getter
+@Setter
+@SessionScoped
+public class ReportBean implements Serializable {
+
+    private long init = 0L, end = 0L;
+    Date fechaRecogida = new Date(System.currentTimeMillis());
+
+    private StreamedContent downloadFile(String endpoint, String filename) {
+        try {
+            String apiUrl = "http://localhost:8081/api/reportes/" + endpoint;
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            // Verificar el código de respuesta
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                "Reporte no generado",
+                                "No se encontraron elementos con los criterios de búsqueda indicados."));
+                return null;
+            }
+
+            InputStream stream = connection.getInputStream();
+
+            return DefaultStreamedContent.builder()
+                    .name("reporte_" + filename + ".pdf")
+                    .contentType("application/pdf")
+                    .stream(() -> stream)
+                    .build();
+
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Error al descargar el archivo",
+                            e.getMessage()));
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public StreamedContent getDatosCultivos() {
+        return downloadFile("cultivos", "resumen_cultivos");
+    }
+
+    public StreamedContent getPlanProduccion() {
+        return downloadFile("planProdBetween/" + init + "/" + end, "cultivos_por_plan_produccion");
+    }
+
+    public StreamedContent getProdCultivosPermanenteAfter() {
+        return downloadFile("prodCultivosPermanenteAfter/" + init, "cultivos_prod_cultivo_permanente");
+    }
+
+    public StreamedContent getFechaRecogidaBefore() {
+        if(fechaRecogida == null)
+            fechaRecogida = new Date(System.currentTimeMillis());
+        SimpleDateFormat formateador = new SimpleDateFormat("dd-MM-yyyy");
+        String date = formateador.format(fechaRecogida);
+        return downloadFile("fechaRecogidaBefore/" + date, "cultivos_fecha_recogidaBefore");
+    }
+}
